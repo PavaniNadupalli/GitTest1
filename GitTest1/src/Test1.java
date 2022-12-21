14819ac5cacc475ec408c0b738993fac08c80dfc
PavaniNadupalli <lalithaPavani24893@gmail.com>
2022-12-21 10:27:35 +0530

    first commit

---
+++ GitTest1/src/Test1.java
@@ -0,0 +1,20 @@
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String hungry = "yes";
+		
+		if(hungry == "yes")
+		{
+			System.out.println("have a nice food");
+		}
+		else
+		{
+			System.out.println("enjoy your day");
+		}
+
+
+	}
+
+}
